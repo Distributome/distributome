2bce70e877b959603b213a823f9b454a08c73eea
iwaterpolo <iwaterpolo@f0c78c53-beb6-468e-edc4-e3df8edcfdfe>
2012-05-26 00:07:15 +0000

    fixed a bug with HyperbolicSecantCalculator

--- js/calc/HyperbolicSecantCalculator.js
+++ js/calc/HyperbolicSecantCalculator.js
@@ -1,4 +1,4 @@
-//Arcsine distribution calculator
+//Hyperbolic Secant distribution calculator
 var dist, distGraph;
 var xParam, pParam, graphSelect;
 var x, p = 0.5;
@@ -7,14 +7,24 @@ function initialize(){
 	distCanvas = document.getElementById("distCanvas");
 	graphSelect = document.getElementById("graphSelect");
 	distSelect = document.getElementById("distSelect");
+	
+	muParam = new Parameter(document.getElementById("muInput"), document.getElementById("muLabel"));
+	muParam.setProperties(-50, 50, 0.1, 0, "<var>\u03BC</var>");
+	sigmaParam = new Parameter(document.getElementById("sigmaInput"), document.getElementById("sigmaLabel"));
+	sigmaParam.setProperties(1, 50, 0.1, 1, "<var>\u03C3</var>");
+
+	xParam = new Parameter(document.getElementById("xInput"), document.getElementById("xLabel"));
 	pParam = new Parameter(document.getElementById("pInput"), document.getElementById("pLabel"));
 	pParam.setProperties(0.001, 0.999, 0.001, 0.5, "<var>p</var>");
-	dist = new HyperbolicSecantDistribution();
+	setDist();	
+}
+
+function setDist(){
+	dist = new HyperbolicSecantDistribution(muParam.getValue(), sigmaParam.getValue());
+	xParam.setProperties(dist.quantile(0.001), dist.quantile(0.999), 0.001, dist.quantile(0.5), "<var>x</var>");
 	distGraph = new QuantileGraph(distCanvas, dist, "X");
 	distGraph.xFormat = 2;
 	distGraph.setGraphType(graphSelect.value);
-	xParam = new Parameter(document.getElementById("xInput"), document.getElementById("xLabel"));
-	xParam.setProperties(0.001, 0.999, 0.001, 0.5, "<var>x</var>");
 	setProb();	
 }
 
