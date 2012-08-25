f070c7c102b7e8ff42387a24036c3472acc5efc2
iwaterpolo <iwaterpolo@f0c78c53-beb6-468e-edc4-e3df8edcfdfe>
2012-08-25 22:44:00 +0000

    Added a new Distributome.xml.Book.pref file that includes alternative hierarchical classification of the probability distirbutions according to the commonly used Probability textbooks.
    Added the following new probability distribution calculators:
    	<hierarchy_level id="top_hierarchy">
    		<name>Bernoulli</name>
    		<name>Binomial</name>
    		<name>Geometric</name>
    		<name>Discrete Uniform</name>
    		<name>Hypergeometric</name>
    		<name>Negative-Binomial</name>
    		<name>Multinomial</name>
    		<name>Poisson</name>
    	</hierarchy_level>
    
    	<hierarchy_level id="middle_hierarchy">
    		<name>Student</name>
    		<name>Normal</name>
    		<name>Beta</name>
    		<name>Cauchy</name>
    		<name>Continuous Uniform</name>
    		<name>Gamma</name>
    		<name>Pareto</name>
    		<name>ChiSquare</name>
    		<name>F</name>
    		<name>Exponential</name>
    	</hierarchy_level>

---
+++ js/calc/ExponentialCalculator.js
@@ -0,0 +1,44 @@
+//Exponential Calculator
+var dist, distGraph, kParam, bParam;
+var xParam, pParam, graphSelect;
+var x, p;
+
+function initialize(){
+	distCanvas = document.getElementById("distCanvas");
+	graphSelect = document.getElementById("graphSelect");
+	distSelect = document.getElementById("distSelect");
+	
+	rateParam = new Parameter(document.getElementById("rateInput"), document.getElementById("rateLabel"));
+	rateParam.setProperties(1, 50, 0.1, 1, "<var>rate</var>");
+	
+	xParam = new Parameter(document.getElementById("xInput"), document.getElementById("xLabel"));
+	pParam = new Parameter(document.getElementById("pInput"), document.getElementById("pLabel"));
+	pParam.setProperties(0.001, 0.999, 0.001, 0.5, "<var>p</var>");
+	setDist();
+}
+
+function setDist(){
+	dist = new ExponentialDistribution(rateParam.getValue());
+	xParam.setProperties(dist.quantile(0.001), dist.quantile(0.999), 0.001, dist.quantile(0.5), "<var>x</var>");
+	distGraph = new QuantileGraph(distCanvas, dist, "X");
+	distGraph.xFormat = 2;
+	distGraph.setGraphType(graphSelect.value);
+	setProb();	
+}
+
+function setValue(){
+	x = xParam.getValue();
+	p = dist.CDF(x);
+	pParam.setValue(p);
+	distGraph.setValue(x);
+}
+
+function setProb(){
+	p = pParam.getValue();
+	x = dist.quantile(p);
+	xParam.setValue(x);
+	distGraph.setProb(p);
+}
+
+
+
