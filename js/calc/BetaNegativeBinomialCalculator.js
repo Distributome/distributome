0d3c0f8028c1f527ed825ba00e076afd04ac559a
iwaterpolo <iwaterpolo@f0c78c53-beb6-468e-edc4-e3df8edcfdfe>
2012-12-17 02:00:24 +0000

    (December 2012) Updated experiments, simulators, and calculators.  The html files have the proper headers and footers, and we purged some obsolete files.
    TO DO!!!
    
    We need to reconsider these tools.  Every distribution can have a simple calculators, but simulators and experiments are not as well defined. Experiments are simulations, just showing some additional information. Conversely, every simulator could be an experiment by showing the method of
    simulation (random quantile, rejection method, simulation in terms of simpler distributions, etc.)
    
    Some distributions have several natural simulators, others only one or two. The binomial can be illustrated in terms of coins, a Galton board, sampling with replacement, a Bernoulli trials "timeline", etc. For the normal distribution, the standard method simulation is via polar coordinates, with an exponentially distributed radius and a uniformly distributed angle.  The simulation could show this graphically.
    
    A suggestion is to reorganize all 3 types of tools, at the top level, not by type (since again, the types don't make sense to me) but rather by distribution. For each distribution, we would have a calculator and at least one (often several) simulators.  In the Navigator, each distribution could be linked to the set of tools for that distribution.
    
    We need to enhance the calculators to display the mean, standard deviation, skewness and kurtosis for each distribution, according to the distributome.xml DB.

--- js/calc/BetaNegativeBinomialCalculator.js
+++ js/calc/BetaNegativeBinomialCalculator.js
@@ -1,43 +1,48 @@
-//Cauchy distribution calculator
-var dist, distGraph, aParam, bParam;
-var xParam, qParam, graphSelect;
-var x, q;
+//Beta-negative binomial distribution calculator
+var dist, distGraph;
+var xParam, pParam, graphSelect;
+var k = 1, a = 3, b = 3, kParam, aParam, bParam;
+var x, p = 0.5;
 
 function initialize(){
 	distCanvas = document.getElementById("distCanvas");
 	graphSelect = document.getElementById("graphSelect");
 	distSelect = document.getElementById("distSelect");
 	aParam = new Parameter(document.getElementById("aInput"), document.getElementById("aLabel"));
-	aParam.setProperties(-10, 10, 0.1, 0, "<var>a</var>");
+	aParam.setProperties(3, 10, 0.1, a, "<var>a</var>");
 	bParam = new Parameter(document.getElementById("bInput"), document.getElementById("bLabel"));
-	bParam.setProperties(0.5, 10, 0.1, 1, "<var>b</var>");
+	bParam.setProperties(3, 10, 0.1, b, "<var>b</var>");	
+	kParam = new Parameter(document.getElementById("kInput"), document.getElementById("kLabel"));
+	kParam.setProperties(1, 10, 1, k, "<var>k</var>");
+	pParam = new Parameter(document.getElementById("pInput"), document.getElementById("pLabel"));
+	pParam.setProperties(0.001, 0.999, 0.001, 0.5, "<var>p</var>");
 	xParam = new Parameter(document.getElementById("xInput"), document.getElementById("xLabel"));
-	qParam = new Parameter(document.getElementById("qInput"), document.getElementById("qLabel"));
-	qParam.setProperties(0.001, 0.999, 0.001, 0.5, "<var>q</var>");
-	setDist();
+	setDist();	
 }
 
 function setDist(){
-	dist = new CauchyDistribution(aParam.getValue(), bParam.getValue());
+	a = aParam.getValue();
+	b = bParam.getValue();
+	k = kParam.getValue();
+	dist = new BetaNegativeBinomialDistribution(a, b, k);
 	xParam.setProperties(dist.quantile(0.001), dist.quantile(0.999), 0.001, dist.quantile(0.5), "<var>x</var>");
 	distGraph = new QuantileGraph(distCanvas, dist, "X");
-	distGraph.xFormat = 2;
 	distGraph.setGraphType(graphSelect.value);
 	setProb();	
 }
 
 function setValue(){
 	x = xParam.getValue();
-	q = dist.CDF(x);
-	qParam.setValue(q);
+	p = dist.CDF(x);
+	pParam.setValue(p);
 	distGraph.setValue(x);
 }
 
 function setProb(){
-	q= qParam.getValue();
-	x = dist.quantile(q);
+	p = pParam.getValue();
+	x = dist.quantile(p);
 	xParam.setValue(x);
-	distGraph.setProb(q);
+	distGraph.setProb(p);
 }
 
 
--- js/calc/ExponentialLogarithmicCalculator.js
+++ js/calc/ExponentialLogarithmicCalculator.js
@@ -1,7 +1,7 @@
 //Exponential-Logarithmic Distribution Calculator
-var dist, distGraph, bParam, qParam;
-var xParam, pParam, graphSelect;
-var b = 1, q = 0.5;
+var dist, distGraph, bParam, pParam;
+var xParam, qParam, graphSelect;
+var b = 1, p = 0.5;
 var x, p;
 
 function initialize(){
@@ -10,35 +10,34 @@ function initialize(){
 	distSelect = document.getElementById("distSelect");
 	bParam = new Parameter(document.getElementById("bInput"), document.getElementById("bLabel"));
 	bParam.setProperties(1, 10, 0.1, b, "<var>b</var>");
-	qParam = new Parameter(document.getElementById("qInput"), document.getElementById("qLabel"));
-	qParam.setProperties(0.1, 0.9, 0.01, q, "<var>q</var>");
-	xParam = new Parameter(document.getElementById("xInput"), document.getElementById("xLabel"));
 	pParam = new Parameter(document.getElementById("pInput"), document.getElementById("pLabel"));
-	pParam.setProperties(0.001, 0.999, 0.001, 0.5, "<var>p</var>");
+	pParam.setProperties(0.1, 0.9, 0.01, p, "<var>p</var>");
+	xParam = new Parameter(document.getElementById("xInput"), document.getElementById("xLabel"));
+	qParam = new Parameter(document.getElementById("qInput"), document.getElementById("qLabel"));
+	qParam.setProperties(0.001, 0.999, 0.001, 0.5, "<var>q</var>");
 	setDist();
 }
 
 function setDist(){
-	q = qParam.getValue();
+	p = pParam.getValue();
 	b = bParam.getValue();
-	dist = new ExponentialLogarithmicDistribution(q, b);
+	dist = new ExponentialLogarithmicDistribution(p, b);
 	xParam.setProperties(dist.quantile(0.001), dist.quantile(0.999), 0.001, dist.quantile(0.5), "<var>x</var>");
 	distGraph = new QuantileGraph(distCanvas, dist, "X");
-	distGraph.xFormat = 2;
 	distGraph.setGraphType(graphSelect.value);
 	setProb();	
 }
 
 function setValue(){
 	x = xParam.getValue();
-	p = dist.CDF(x);
-	pParam.setValue(p);
+	q = dist.CDF(x);
+	qParam.setValue(q);
 	distGraph.setValue(x);
 }
 
 function setProb(){
-	p = pParam.getValue();
-	x = dist.quantile(p);
+	q = qParam.getValue();
+	x = dist.quantile(q);
 	xParam.setValue(x);
-	distGraph.setProb(p);
+	distGraph.setProb(q);
 }
